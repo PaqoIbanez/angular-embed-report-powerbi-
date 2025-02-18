72ed897bcdab71d2d6e9d1cca4ca27348f3a473c
Francisco Javier Ibanez Canales <francisco.paqo@gmail.com>
2025-02-18 05:38:47 -0600

    Log debugging url and errors

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -15,23 +15,25 @@ export class AuthInterceptor implements HttpInterceptor {
 
   constructor() { }
 
-  intercept( request: HttpRequest<unknown>, next: HttpHandler ): Observable<HttpEvent<unknown>> {
-    return next.handle( request ).pipe(
-      catchError( ( error: HttpErrorResponse ) => {
-        if (
-          error.status === 401 &&
-          !request.url.endsWith( '/login' ) &&
-          !request.url.endsWith( '/logout' ) &&
-          !request.url.endsWith( '/check' )
-        ) {
-          console.log( 'AuthInterceptor: 401 error detected for URL:', request.url );
-          console.log( 'AuthInterceptor: Emitting logoutRequest event' );
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    console.log(`AuthInterceptor: Intercepting request to ${request.url}`); // Log the URL
+
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.log(`AuthInterceptor: Error response received for ${request.url}`, error); // Log the error
+
+        if (error.status === 401 &&
+          !request.url.endsWith('/login') &&
+          !request.url.endsWith('/logout') &&
+          !request.url.endsWith('/check')) {
+          console.warn(`AuthInterceptor: 401 error detected for URL: ${request.url}`);
+          console.warn('AuthInterceptor: Emitting logoutRequest event');
           this.logoutRequest.emit();
-        } else if ( error.status === 401 ) {
-          console.log( 'AuthInterceptor: 401 error, but URL is excluded from logout:', request.url );
+        } else if (error.status === 401) {
+          console.warn(`AuthInterceptor: 401 error, but URL is excluded from logout: ${request.url}`);
         }
-        return throwError( () => error );
-      } )
+        return throwError(() => error);
+      })
     );
   }
 }
